df5fa41afab0285d4916074982f09471898603e3
Julien Dewost <dewostjulien@gmail.com>
2020-01-29 16:43:39 +0100

    heritage

--- app.js
+++ app.js
@@ -64,5 +64,55 @@ parisTrip.price=100;
 //console.log(parisTrip.toString());
 
 const defaultTrip=Trip.getDefaultTrip();
-console.log(defaultTrip.toString());
+//console.log(defaultTrip.toString());
 
+class FreeTrip extends Trip{
+    constructor(id, name, imageUrl) {
+        super(id, name, imageUrl);
+        this.price = 0;
+    }
+
+    toString(){
+        return "Free"+super.toString();
+    }
+}
+
+const freeTrip = new FreeTrip('nantes','nantes','img/nantes.jpg');
+console.log(freeTrip.toString());
+
+
+class TripService {
+    constructor() {
+    // TODO Set of 3 trips
+    //
+    // new Trip('paris', 'Paris', 'img/paris.jpg')
+    // new Trip('nantes', 'Nantes', 'img/nantes.jpg')
+    // new Trip('rio-de-janeiro', 'Rio de Janeiro', 'img/rio-de-janeiro.jpg')
+    }
+    findByName(tripName) {
+    return new Promise((resolve, reject) => {
+    setTimeout( () => {
+    // ici l'exécution du code est asynchrone
+    // TODO utiliser resolve et reject en fonction du résultat de la
+  recherche
+    }, 2000)
+    });
+    }
+  }
+  class PriceService {
+    constructor() {
+    // TODO Map of 2 trips
+    // 'paris' --> price == 100
+    // 'rio-de-janeiro' --> price == 800)
+    // no price for 'nantes'
+    }
+    findPriceByTripId(tripId) {
+    return new Promise((resolve, reject) => {
+    setTimeout( () => {
+    // ici l'exécution du code est asynchrone
+    // TODO utiliser resolve et reject en fonction du résultat de la recherche
+    }, 2000)
+    });
+    }
+  }
+  
