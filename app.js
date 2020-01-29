a4e99fdf602ea5b740f6534bbf0c537f8d4a098a
Julien Dewost <dewostjulien@gmail.com>
2020-01-29 14:44:38 +0100

    affectation destructuree

--- app.js
+++ app.js
@@ -4,8 +4,25 @@ favoriteCityId="paris";
 //console.log(favoriteCityId);
 
 const citiesId =["paris", "nyc", "rome", "rio-de-janeiro"];
-console.log(citiesId);
+//console.log(citiesId);
 //Assignment to constant variable.
 //citiesId=[];
 citiesId.push("tokyo");
-console.log(citiesId);
+//console.log(citiesId);
+
+function getWeather(citiesId){
+    let city=citiesId.toUpperCase();
+    let temperature =20;
+    return {city:city,temperature:temperature};
+    
+}
+const weather = getWeather(favoriteCityId);
+//console.log(weather);
+
+let {
+    city: nomCity,
+    temperature: numTemperature
+  } = weather;
+
+  console.log(nomCity); // Robert
+  console.log(numTemperature);
