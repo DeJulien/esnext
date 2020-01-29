b6ef02174a3e2f018e5b155f7324ce4959d4f724
Julien Dewost <dewostjulien@gmail.com>
2020-01-29 16:35:03 +0100

    class

--- app.js
+++ app.js
@@ -29,10 +29,40 @@ let {
 
 
 let [parisId,nycId,...othersCitiesId] = citiesId;
-console.log(parisId);
-console.log(nycId);
-console.log(othersCitiesId.length);
+//console.log(parisId);
+//console.log(nycId);
+//console.log(othersCitiesId.length);
 
 class Trip{
-    
+    constructor(id, name, imageUrl) {
+        this.id = id;
+        this.name = name;
+        this.imageUrl=imageUrl;
+        }
+    get price(){
+        return this._aprice;
+    }
+    set price(newPrice) {
+        this._price = newPrice;
+    }
+    static getDefaultTrip(
+        id = 'rio-de-janeiro',
+        name = 'Rio de Janeiro',
+        imageUrl = 'img/rio-de-janeiro.jpg') {
+        return new Trip(id, name, imageUrl);
+    }
+        
+    toString(){
+        return "Trip ["+this.id+","+this.name+","+this.imageUrl+","+this._price+"]";
+    }
+
 }
+let parisTrip = new Trip('paris','paris','img/paris.jpg');
+//console.log(parisTrip);
+//console.log(parisTrip.name);
+parisTrip.price=100;
+//console.log(parisTrip.toString());
+
+const defaultTrip=Trip.getDefaultTrip();
+console.log(defaultTrip.toString());
+
